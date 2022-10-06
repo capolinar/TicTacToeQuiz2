b2e17f7947be3916e93d12a2ac3c47646f27826d
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-05 19:42:45 -0700

    Added more text

--- TicTacToe.c
+++ TicTacToe.c
@@ -6,7 +6,12 @@ int main(){
     //prompt user for game they wish to play
     printf("Welcome to Tic Tac Toe! \n"); 
     printf("1---person vs person \n \n");
-    printf("2---person vs. random computer \n");
+    printf("2---person vs. random computer \n \n");
+    printf("Enter your choice(1 or 2): \n");
+    //code ask for choice
+    printf("=============================\n\n");
+    printf("You have entered choice ");
+    //print the choice made
 
     
 
