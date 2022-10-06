d1ab27bca9ae41832b51dfcc79e9b8e49ac94c31
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-05 19:28:40 -0700

    Added prompt to ask user what game they want to play

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,10 +1,13 @@
 //Tic Tac Toe between 2 players or player vs computer
+#include <stdio.h>
 
 
 int main(){
     //prompt user for game they wish to play
+    printf("Welcome to Tic Tac Toe! \n"); 
+    printf("1---person vs person \n \n");
+    printf("2---person vs. random computer \n");
 
-    
     
 
     //read information from console
