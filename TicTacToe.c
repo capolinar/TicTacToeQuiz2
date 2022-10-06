9a4db4765a285cb088a25b7efe700ecc162dfdba
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-05 19:18:09 -0700

    Added comments

---
+++ TicTacToe.c
@@ -0,0 +1,15 @@
+//Tic Tac Toe between 2 players or player vs computer
+
+
+int main(){
+    //prompt user for game they wish to play
+    
+
+    //read information from console
+
+    //asdfasdf
+
+
+
+    return 0;
+}
