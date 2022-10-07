4112561ff721fa4100b1b77ee35ba89e1f50a98b
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-06 17:47:02 -0700

    Added tic tac toe board print

--- TicTacToe.c
+++ TicTacToe.c
@@ -15,10 +15,19 @@ int main(){
     printf("You have entered choice %d", i); 
     printf("\n\nYour Current status is: \n\n");
 
+    printf("+-----------+\n");
+    printf("|   |   |   | \n");
+    printf("+-----------+\n");
+    printf("|   |   |   | \n");
+    printf("+-----------+\n");
+    printf("|   |   |   | \n");
+    printf("+-----------+\n\n");
+    printf("Player1: make your move\n");
+
     //game if i = 1(person vs. person)
     if(i == 1)
     {
-//add game
+    //add game
     }
 
     //game if i = 2(random computer)
