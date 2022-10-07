e021b078a74a84da2dbef293c07f1b05f897a98d
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-06 18:45:10 -0700

    Added function win() that checks the progress of thegame

--- TicTacToe.c
+++ TicTacToe.c
@@ -22,6 +22,44 @@ void printBoard()
     printf("+-----------+\n");
 }
 
+int win() //function that returns 1 if game is over, -1 if in progress, and 0 if over and no result(TIE)
+{                                                             //each of these check if X or O are in a row
+                                                        //essentially checks all combinations of win conditions
+   
+    if (square[1] == square[2] && square[2] == square[3]) //top row
+        return 1;
+        
+    else if (square[4] == square[5] && square[5] == square[6])//middle row
+        return 1;
+        
+    else if (square[7] == square[8] && square[8] == square[9])//bottom row
+        return 1;
+        
+    else if (square[1] == square[4] && square[4] == square[7])//left column
+        return 1;
+        
+    else if (square[2] == square[5] && square[5] == square[8])//middle column
+        return 1;
+        
+    else if (square[3] == square[6] && square[6] == square[9])//bottom column
+        return 1;
+        
+    else if (square[1] == square[5] && square[5] == square[9])//left diaganol
+        return 1;
+        
+    else if (square[3] == square[5] && square[5] == square[7])//right diagnol
+        return 1;
+        
+    else if (square[1] != ' ' && square[2] != ' ' && square[3] != ' ' && //checks if any remaining spaces
+        square[4] != ' ' && square[5] != ' ' && square[6] != ' ' && square[ ] 
+        != ' ' && square[8] != ' ' && square[9] != ' ')
+
+        return 0; //if no remaining spaces, game is over(tie)
+    else
+        return  - 1; //if there is remaining spaces, game is in progress
+
+}
+
 int main(){
     //prompt user for game they wish to play
     printf("Welcome to Tic Tac Toe! \n"); 
